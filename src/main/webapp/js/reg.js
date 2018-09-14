5acd8f261e595b8c87ad187df01490b587a9cbfe
skyurb <2864814961@qq.com>
2018-09-14 16:10:22 +0800

    test

---
+++ src/main/webapp/js/reg.js
@@ -0,0 +1,22 @@
+function register() {
+
+
+}
+
+function getCode() {
+    var phone = $("#phone").val()
+    console.log(phone);
+
+    $.ajax({
+        url: "/register/getCode.do",
+        type:"post",
+        data:{
+            phoneNumber:phone
+        },
+        dataType:"json",
+        success:function (resp) {
+            alert("发送成功");
+        }
+
+    })
+}
