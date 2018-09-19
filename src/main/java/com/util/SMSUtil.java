4dd76fb8a48ee97bb2eb786ba060199026976c17
skyurb <2864814961@qq.com>
2018-09-19 15:34:42 +0800

    Update SMSUtil.java

--- src/main/java/com/util/SMSUtil.java
+++ src/main/java/com/util/SMSUtil.java
@@ -22,7 +22,7 @@ public class SMSUtil {
     static final String domain = "dysmsapi.aliyuncs.com";
 
     // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-    static final String accessKeyId = "******";
+    static final String accessKeyId = "*****8*";
     static final String accessKeySecret = "********";
 
 
